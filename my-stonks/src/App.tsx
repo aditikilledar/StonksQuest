042d4901f42f2750a085e709985fbddda9a457a2
Aditi Killedar <akilled@ncsu.edu>
2024-11-02 15:00:06 -0400

    created basic nes template

--- my-stonks/src/App.tsx
+++ my-stonks/src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'nes.css/css/nes.min.css';
 import './App.css';
 import Portfolio from './portfolio/Portfolio';
 
 function App() {
   return (
     <div className="container">
-      <div className="section section1">Section 1</div>
-      <div className="section section2">Section 2</div>
-      <div className="section section3">Section 3</div>
-      <div className="section section4">
+      <header className="slogan nes-container is-dark">
+        <h1 className="slogan-text">goal??</h1>
+      </header>
+      <div className="nes-container with-title">
+        <h3 className='title'>Stocks View</h3><p className='nes-text is-primary'>Section 1</p>
+      </div>
+      <div className="nes-container with-title">
+        <h3 className='title'>Bank</h3><p className='nes-text is-primary'>Section 1</p>
+      </div>
+      <div className="nes-container with-title">
+        <h3 className='title'>Buy Stocks</h3><p className='nes-text is-primary'>Section 1</p>
+      </div>
+      <div className="nes-container with-title">
+        <h3 className='title'>Portfolio</h3><p className='nes-text is-primary'>Section 1</p>
         <Portfolio />
       </div>
     </div>
