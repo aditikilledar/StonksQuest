7955939a9d3f52599fec44288c5c7322110d279c
Aditi Killedar <akilled@ncsu.edu>
2024-11-02 14:42:28 -0400

    Divided into 4 sections

--- my-stonks/src/App.tsx
+++ my-stonks/src/App.tsx
@@ -5,9 +5,13 @@ import Portfolio from './portfolio/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-
-      <Portfolio />
+    <div className="container">
+      <div className="section section1">Section 1</div>
+      <div className="section section2">Section 2</div>
+      <div className="section section3">Section 3</div>
+      <div className="section section4">
+        <Portfolio />
+      </div>
     </div>
 
   );
